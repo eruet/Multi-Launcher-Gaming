6b925cf3fca5e97bbd51070894b4d5bf3f693a44
Enzo Ruet <enzo.ruet@gmail.com>
2019-05-07 13:54:07 +0200

    Ajout de la page index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+
+    <head>
+        <title>Installation</title>
+        <link rel="icon" href="https://enzo-ruet.fr/centralisateur/logo.ico" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    
+    <body style="background-color: #d6d8d9;">
+        <div class="container">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-10" style="border: solid 1px black; top:0; border-radius: 5px; margin-top: 100px; border-color: #c6c8ca; background-color: white;">
+                    <div class="alert alert-dark" role="alert" style="margin-top: 1rem; margin-bottom: 80px;">
+                        <center>
+                            <h3>Installation</h3>
+                        </center>
+                    </div>
+                        <p align="justify">Merci d'indiquer la destination du dossier des jeux d'un distributeur ci-dessous. Voici quelques exemples.<br/>
+                        - Pour Steam : C:\Steam\steamapps\common\<br/>
+                            <b>N'oubliez pas de mettre un "\" à la fin !</b><br/>
+                        </p>
+                        <form>
+                            <input type="text" size="100%" class="form-control" name="destination" placeholder="Mettez ici la destination.">
+                            <input type="submit" class="form-control" value="Je valide" >
+                        </form>
+                        <div class="alert alert-dark" role="alert" style="margin-top: 80px;">
+                            <center>
+                                Créé par Enzo Ruet
+                            </center>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    
+</html>
